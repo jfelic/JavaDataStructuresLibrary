425c2409b1b7e0b0344d17da80f4d984178b043b
Julian Feliciano <82845183+jfelic@users.noreply.github.com>
2024-03-20 00:20:04 -0400

    Update ArrayStack.java
    Added some basic methods

--- src/dslib/stack/ArrayStack.java
+++ src/dslib/stack/ArrayStack.java
@@ -9,8 +9,44 @@ public class ArrayStack<E> implements StackInterface<E> {
 
     public ArrayStack(int maxSize) {
         this.maxSize = maxSize; //set array size
-        stackArray = new E[maxSize]; //create array
+        //Create Array
+        stackArray = ([E]) new Object[maxSize];//we cannot directly create an array of generic type
+                                               //so we need to typecast an Array of Objects to generic
         top = -1; //no items yet
     }
 
+    @Override
+    public void push(E item) {
+        if( isFull() ) {
+            System.out.println("Stack is full, cannot push");
+            return;
+        }
+        stackArray[top + 1] = item;
+        top++;
+    }
+
+    @Override
+    public E pop() {
+        if( isEmpty() ) {
+            System.out.println("Stack is empty, cannot pop");
+            return;
+        }
+        E temp = stackArray[top];
+        top--;
+        return temp;
+    }
+
+    public boolean isFull() {
+        if(top >= maxSize - 1)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isEmpty() {
+        if(top == -1) 
+            return true;
+        else
+            return false;
+    }
 }
